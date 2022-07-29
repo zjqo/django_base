3f4597c8aac628fbe69cfa596dd0e93de1cebff6
zjq <13184548309@163.com>
2022-07-29 07:28:06 -0700

    完成一半的模块迁移

---
+++ bookmanager/book/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
